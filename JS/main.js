dd975ff5a44a27e1d1d6e2d434654f037df64b62
Yangala Sushmasri <sushmasri9947@gmail.com>
2023-04-08 15:08:25 -0400

    Made css changes and added home page

--- JS/main.js
+++ JS/main.js
@@ -12,14 +12,14 @@ signInButton.addEventListener('click', () => {
     container.classList.remove("right-panel-active");
 });
 
-function show_hide_password(target){
-	var input = document.getElementById('password-input');
-	if (input.getAttribute('type') == 'password') {
-		target.classList.add('view');
-		input.setAttribute('type', 'text');
-	} else {
-		target.classList.remove('view');
-		input.setAttribute('type', 'password');
-	}
-	return false;
+function show_hide_password(target) {
+    var input = document.getElementById('password-input');
+    if (input.getAttribute('type') == 'password') {
+        target.classList.add('view');
+        input.setAttribute('type', 'text');
+    } else {
+        target.classList.remove('view');
+        input.setAttribute('type', 'password');
+    }
+    return false;
 }
