f3877f4ee9bb33a0fbc8db1c75c43da4afdcbf5e
Yangala Sushmasri <sushmasri9947@gmail.com>
2023-04-12 13:55:41 -0400

    Added Sidenav bar

--- PHP/login.php
+++ PHP/login.php
@@ -4,16 +4,16 @@ extract($_POST);
 if(isset($_POST['login']))
 {
 $email = $_POST['email'];
+$password = $_POST['password'];
 $sql = "SELECT * FROM users WHERE email='$email'";
 $result = $conn->query($sql);
 $row = $result->fetch_assoc();
-$password = $_POST['password'];
 if (password_verify($password, $row['password_hash'])) {
   $_SESSION['user'] = $row['username'];
-  header('Location: ./Home.html');
+  header('Location: ../Welcome.html');
   exit;
 } else {
-  header('Location: ./login.html');
+  header('Location: ../login.html');
   exit;
 }
 }
